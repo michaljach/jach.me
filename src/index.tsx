b9f397ae255a4c8bd837214ca4c49a3b35da7657
Michael Jach <michaljach@gmail.com>
2024-01-21 13:56:04 +0100

    privacy policy

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { Component, render, Router } from "htm0";
 import HomePage from "./pages/home/home";
 import ItemsPage from "./pages/items/items";
 import Year2023Page from "./pages/2023/2023";
+import PrivacyPage from "./pages/privacy/privacy";
 
 class AppComponent extends Component {
   render() {
@@ -11,6 +12,7 @@ class AppComponent extends Component {
           "/": <HomePage />,
           "/items": <ItemsPage />,
           "/2023": <Year2023Page />,
+          "/privacy": <PrivacyPage />,
         }}
       />
     );
